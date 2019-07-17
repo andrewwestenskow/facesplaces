290e5b6dad0aab20c3cc06d8ff17191a28ed704e
Andrew Westenskow <andrew.westenskow@gmail.com>
2019-07-17 14:25:27 -0600

    initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+
+const App = (props) => {
+  return (
+    <Text>App</Text>
+)
+}
+export default App
+
+const styles = StyleSheet.create({
+
+})
