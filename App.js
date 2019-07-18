fc834cb6d17582e6711fe073b203fb72bd406b4a
Andrew Westenskow <andrew.westenskow@gmail.com>
2019-07-18 13:46:37 -0600

    Added permission request

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet, Button} from 'react-native'
-
-
+import { View, Text, StyleSheet, Button, PermissionsAndroid } from 'react-native'
 
 
 class App extends Component {
@@ -10,17 +8,43 @@ class App extends Component {
     position: ''
   }
 
+  async componentDidMount(){
+    await this.getPermission()
+  }
+
+  getPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          'title': 'Example App',
+          'message': 'Example App access to your location '
+        }
+      )
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log("You can use the location")
+        alert("You can use the location");
+      } else {
+        console.log("location permission denied")
+        alert("Location permission denied");
+      }
+    } catch (err) {
+      console.warn(err)
+    }
+  }
+
   getLocation = () => {
     //This is where I'm trying to get location
-    navigator.geolocation.getCurrentPosition(position=>{
+
+    navigator.geolocation.getCurrentPosition(position => {
       console.log(position)
-    }, err=>console.log(err))
+    }, err => console.log(err))
   }
   render() {
     return (
       <View>
         <Text>Andrew</Text>
-        <Button title='get location' onPress={this.getLocation}/>
+        <Button title='get location' onPress={this.getLocation} />
       </View>
     )
   }
